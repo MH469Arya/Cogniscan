852e1e317b5af9838a8b5ed881559e67cbb14497
Arya Mangesh Hegiste <arya19.hegiste9a@gmail.com>
2026-03-29 21:57:30 +0530

    Commit

--- cogniscan_call/elderly-voice-assistant/call_me.py
+++ cogniscan_call/elderly-voice-assistant/call_me.py
@@ -19,16 +19,16 @@
 #     print(f"❌ Error: {e}")
 from twilio.rest import Client
 
-account_sid = 'ACaad6212f586a4cd6393310a6918e3aac'
-auth_token = 'f9f0f170df701a2dff86b94bfc4eef50'
+account_sid = '---SECRET KEY---'
+auth_token = '---SECRET KEY---'
 
 client = Client(account_sid, auth_token)
 
 NGROK_URL = 'https://ross-unexploitative-unhinderably.ngrok-free.dev'
 
 call = client.calls.create(
-    to='+918591151909',
-    from_='+14788003151',
+    to='PhoneNumber',  # Replace with the actual phone number you want to call
+    from_='PhoneNumber',  # Replace with your Twilio phone number
     url=f'{NGROK_URL}/voice'
 )
 
